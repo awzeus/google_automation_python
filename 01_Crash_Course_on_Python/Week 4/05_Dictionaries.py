d6b18b3b226dbf4b75761529a699b63cf7844044
Cesar Holguin <cesarhgn@hotmail.com>
2021-06-25 16:58:55 -0700

    Adding to dictionaries

--- 01_Crash_Course_on_Python/Week 4/05_Dictionaries.py
+++ 01_Crash_Course_on_Python/Week 4/05_Dictionaries.py
@@ -4,3 +4,11 @@ toc["Epilogue"] = 39 # Epilogue starts on page 39
 toc["Chapter 3"] = 24 # Chapter 3 now starts on page 24
 print(toc) # What are the current contents of the dictionary?
 print("Chapter 5" in toc) # Is there a Chapter 5?
+
+
+
+# Complete the code to iterate through the keys and values of the cool_beasts dictionary. 
+# Remember that the items method returns a tuple of key, value for each element in the dictionary.
+cool_beasts = {"octopuses":"tentacles", "dolphins":"fins", "rhinos":"horns"}
+for key, value in cool_beasts.items():
+    print("{} have {}".format(key,value))
